bd9714db7f53ccbd160e74b680b0d06e2ea8b499
RedstoneParadox <spinjitzu00@gmail.com>
2019-03-29 18:44:05 -0700

    Lexer
    Work on the new lexer/tokenizer is almost complete.

---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+use std::error::Error;
+use std::fs;
+use std::env;
+use std::process;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments.")
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config { query, filename, case_sensitive })
+    }
+}
+
+pub fn run_old(config : Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    return results
+}
+
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    return results
+}
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::process;
+
+use Silicon;
+use Silicon::Config;
+use std::path::PathBuf;
+
+mod engine;
+mod ast;
+mod tokenizer;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() >= 2 {
+        engine::run(PathBuf::from(&args[1]))
+    }
+    else {
+        println!("Please specify the path to the main script!");
+        process::exit(1);
+    }
+
+    println!("Program execution finished successfully.");
+}
